da5b9fb13dace2a592847f62b054be99c683de06
Student User <student@workstation.lab.example.com>
2021-12-22 01:15:09 -0500

    Update the version

--- java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
+++ java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
@@ -11,7 +11,7 @@ public class ServerHostEndPoint {
 
   @GET
   @Produces("text/plain")
-  public Response doGet() {
+ public Response doGet() {
     String host = "";
     try {
       host = InetAddress.getLocalHost().getHostName();
@@ -19,7 +19,7 @@ public class ServerHostEndPoint {
     catch (Exception e) {
        e.printStackTrace();
     }
-    String msg = "I am running on server "+host+" Version 1.0 \n";
+    String msg = "I am running on server "+host+" Version .0 \n";
     return Response.ok(msg).build();
   }
 }
